91862c09c0320812318af6df3eb761f1d306ca60
ken <work970390@gmail.com>
2024-09-10 00:50:57 +0800

    consume declare exchange

--- RabbitMqCustomer/Program.cs
+++ RabbitMqCustomer/Program.cs
@@ -6,6 +6,7 @@ const string queueName = "DirectQueue1";
 
 var rabbitMq = new RabbitMqFactory(queueName);
 await rabbitMq.InitAsync();
+await rabbitMq.DeclareExchangeAsync();
 await rabbitMq.Consume(queueName);
 
 Console.WriteLine("接收訊息");
--- RabbitMqCustomer2/Program.cs
+++ RabbitMqCustomer2/Program.cs
@@ -6,6 +6,7 @@ const string queueName = "DirectQueue2";
 
 var rabbitMq = new RabbitMqFactory(queueName);
 await rabbitMq.InitAsync();
+await rabbitMq.DeclareExchangeAsync();
 await rabbitMq.Consume(queueName);
 
 Console.WriteLine("接收訊息");
